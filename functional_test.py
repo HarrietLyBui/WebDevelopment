928584349345d960aecd0aaf7b6a7b7affd119dd
Demo User <demouser@MacBook-Pro.local>
2016-01-05 11:23:09 -0500

    Expanded the implementation of Edith\'s Functional test

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase): #extend unittest
@@ -21,7 +22,7 @@ class NewVisitorTest(unittest.TestCase): #extend unittest
 
         self.browser.get('http://localhost:8000') #go to a homepage
 
-        self.assertIn('To-Do',self.browser.title)
+
 
         #assert 'To-Do' in browser.title
         #this code does the same thing with line 5
@@ -32,12 +33,34 @@ class NewVisitorTest(unittest.TestCase): #extend unittest
         #She notices the page title and header mention to-do list
         #assert 'To-Do' in browser.title #make Django show up on the page
 
+        self.assertIn('To-Do',self.browser.title)
+        #find the h1 text on the text, got the text and look for To-do word
+        #in the text
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+
         #She is invited to endter a to-do item straight away
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         #Shy types "Buy peacock feathers" into a text box
         #(Edith's hobby is tying fly-fishing lures)
 
+        inputbox.send_keys('Buy peacock feathers')
+
         #when she hits enter, the page udates, and now the page lists
         # 1. Buy peacock feathers" as an item in a to-do lists
+
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any()
+        )
 
         #There is still a text box inviting her to add aanother item#
         #She enters "Use peacock feathers to make fly"
@@ -53,6 +76,6 @@ class NewVisitorTest(unittest.TestCase): #extend unittest
 
         #Satisfied, she goes back to sleep
         self.fail('Finish the app!')
-        
+
 if __name__== '__main__':
     unittest.main()
