7c1e74e68b8ad962e96d1088fcd88fc80d55ebd2
Demo User <demouser@MacBook-Pro.local>
2016-01-08 11:44:11 -0500

    All unittest work

--- lists/tests.py
+++ lists/tests.py
@@ -85,6 +85,31 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
 
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+    #    other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response,'/lists/%d/' % (correct_list.id,))
+
 class ListViewTest(TestCase):
 
     def test_displays_only_items(self):
@@ -108,6 +133,10 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/%d/' % (new_list.id,))
         self.assertTemplateUsed(response,'list.html')
 
+    def test_passes_correct_list_to_template(self):
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+        self.assertEqual(response.context['list'],correct_list)
 
 
 class ItemAndListModelTest(TestCase):
